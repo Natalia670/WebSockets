a838eb0af339cd0aaae616c1ff2a35bb260f5900
Natalia670 <A01338719@itesm.mx>
2020-11-06 21:47:18 -0600

    inital commit

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+function makeToast(){
+    $.toast({
+        text: 'This is called a toast',
+        position: 'top-right'
+    })
+    console.log('Make toast');
+}
+
+function makeToastMessage(message) {
+    $.toast({
+      text: message,
+      position: 'top-right'
+    });
+  }
+    
+
+window.socket = null;
+function connectToSocketIo() {
+  let server = window.location.protocol + "//" + window.location.host;
+  window.socket = io.connect(server);
+  // Recibe un mensaje de tipo toast
+  window.socket.on('toast', function (data) {
+    // Muestra el mensaje
+    makeToastMessage(data.message);
+  });
+}
+
+function emitEventToSocketIo() {
+  let text = $('#messageToServer').val();
+  // Envía un mensaje
+  window.socket.emit('messageToServer', { text: text });
+}
+
+$(function () {
+  connectToSocketIo();
+});
